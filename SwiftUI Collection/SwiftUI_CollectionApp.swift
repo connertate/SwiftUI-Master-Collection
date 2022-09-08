a44700cfd8ad5f19083f1fc57507cf9e8d218012
Conner Tate <connertate8@gmail.com>
2022-09-08 14:55:07 -0400

    Initial Commit

---
+++ SwiftUI Collection/SwiftUI_CollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CollectionApp.swift
+//  SwiftUI Collection
+//
+//  Created by Conner Tate on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
